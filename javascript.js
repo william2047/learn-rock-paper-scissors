553d25c0a6449b04fcfe9ae6e21e440d4c28ad66
William Alkhoury <a.william.20047@gmail.com>
2022-07-09 14:11:23 -0400

    solved logical error in rockPaperScissorRound function

--- javascript.js
+++ javascript.js
@@ -6,15 +6,16 @@ function computerPlay(){
 
 function rockPaperScissorRound(playerSelection, computerSelection){
     let winState = 0;
-    if(computerPlay == computerSelection); //checks if both picks are equal (skips steps)
-    else if(playerSelection == 0 ? 2 : playerSelection-1 == computerPlay) winState ++;
+    if(playerSelection == computerSelection); //checks if both picks are equal (skips steps)
+    else if(((playerSelection == 0) ? 2 : playerSelection-1) == computerSelection) winState ++;
     else winState --;
 
     switch(winState){
-        case 0: return "It's a Tie!"; break;
-        case 1: return "You Win! " + pick[playerSelection] + " beats " + pick[computerSelection]; break;
-        case -1: return "You Loose! " + pick[computerSelection] + " beats " + pick[playerSelection]; break;
+        case 0: return console.log("It's a Tie!"); break;
+        case 1: return console.log("You Win! " + pick[playerSelection] + " beats " + pick[computerSelection]); break;
+        case -1: return console.log("You Loose! " + pick[computerSelection] + " beats " + pick[playerSelection]); break;
     }
     return nulll;
 }
 
+rockPaperScissorRound(1,2);
