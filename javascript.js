3e84e32aa74e7063d5144acdc1f256a7c43888c7
William Alkhoury <a.william.20047@gmail.com>
2022-07-10 21:11:02 -0400

    created round function and 5 round game function

--- javascript.js
+++ javascript.js
@@ -1,21 +1,38 @@
-let pick = ["Rock", "Paper", "Scissor"];
+let pick = ["Rock", "Paper", "Scissor"]; //array to map value 0,1,2 to rock, paper, scissor
 
-function computerPlay(){
+function stringPickTranslator(inp){ //converts number 0,1,2 to rock,paper,scissor
+    inp = inp.toUpperCase();
+
+    switch(inp){
+        case "ROCK": return 0; break;
+        case "PAPER": return 1; break;
+        case "SCISSOR": return 2; break;
+        default: return console.log("ERROR: invalid input :()")
+    }
+}
+
+function computerPlay(){ //generate random number for computer
     return Math.floor(Math.random() * (3 - 0));
 }
 
-function rockPaperScissorRound(playerSelection, computerSelection){
+function playRound(playerSelection, computerSelection){ //rock paper scissor round
+
     let winState = 0;
     if(playerSelection == computerSelection); //checks if both picks are equal (skips steps)
     else if(((playerSelection == 0) ? 2 : playerSelection-1) == computerSelection) winState ++;
     else winState --;
 
     switch(winState){
-        case 0: return console.log("It's a Tie!"); break;
-        case 1: return console.log("You Win! " + pick[playerSelection] + " beats " + pick[computerSelection]); break;
-        case -1: return console.log("You Loose! " + pick[computerSelection] + " beats " + pick[playerSelection]); break;
+        case 0: return console.log("It's a Tie!"); break; //tie text
+        case 1: return console.log("You Win! " + pick[playerSelection] + " beats " + pick[computerSelection]); break; //player win text
+        case -1: return console.log("You Loose! " + pick[computerSelection] + " beats " + pick[playerSelection]); break; //player loose text
+        default: return console.log("ERROR somewhere, i dont know; winstate not valide value") //console log for if state is unexpected value
     }
-    return nulll;
+    return null;
 }
 
-rockPaperScissorRound(1,2);
+function game(){ //5 round game
+    for(let i=0; i<5; i++){
+        playRound(stringPickTranslator(window.prompt("Enter Rock Paper Or Scissor!")), computerPlay());
+    }
+}
