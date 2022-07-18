ad9065c8ebd14ed7adde9ddd830573b2d3199d4e
William Alkhoury <a.william.20047@gmail.com>
2022-07-17 22:49:17 -0400

    completed most of text update and 5 round system logic

--- javascript.js
+++ javascript.js
@@ -1,3 +1,10 @@
+let player2Points = 0;
+let player1Points = 0;
+
+const resText = document.querySelector("#text p");
+const player1Score = document.querySelector("#text  #score #player1");
+const player2Score = document.querySelector("#text  #score #player2");
+
 let pick = ["Rock", "Paper", "Scissor"]; //array to map value 0,1,2 to rock, paper, scissor
 
 function stringPickTranslator(inp){ //converts rock, paper, scissor to  number 0,1,2
@@ -15,24 +22,45 @@ function computerPlay(){ //generate random number for computer
     return Math.floor(Math.random() * (3 - 0));
 }
 
-function playRound(playerSelection, computerSelection){ //rock paper scissor round
+function playRound(player1Selection, player2Selection){ //rock paper scissor round
+    player1Selection
+
 
     let winState = 0;
-    if(playerSelection == computerSelection); //checks if both picks are equal (skips steps)
-    else if(((playerSelection == 0) ? 2 : playerSelection-1) == computerSelection) winState ++;
+    if(player1Selection == player2Selection); //checks if both picks are equal (skips steps)
+    else if(((player1Selection == 0) ? 2 : player1Selection-1) == player2Selection) winState ++;
     else winState --;
 
+    /*if(winState == 0){
+        return "It's a Tie!";
+    } else if (winState == 1){
+        playerPoint ++;
+        return "You Win! " + pick[player1Selection] + " beats " + pick[player2Selection];
+    }*/
+
     switch(winState){
-        case 0: return console.log("It's a Tie!"); break; //tie text
-        case 1: return console.log("You Win! " + pick[playerSelection] + " beats " + pick[computerSelection]); break; //player win text
-        case -1: return console.log("You Loose! " + pick[computerSelection] + " beats " + pick[playerSelection]); break; //player loose text
-        default: return console.log("ERROR somewhere, i dont know; winstate not valide value") //console log for if state is unexpected value
+        case 0: return "It's a Tie!"; break; //tie text
+        case 1: player1Points ++; return "You Win! " + pick[player1Selection] + " beats " + pick[player2Selection]; break; //player win text
+        case -1: player2Points ++; return "You Loose! " + pick[player2Selection] + " beats " + pick[player1Selection]; break; //player loose text
+        default: return "ERROR somewhere, i dont know; winstate not valide value" //console log for if state is unexpected value
     }
-    return null;
 }
 
-function game(){ //5 round game
-    for(let i=0; i<5; i++){
-        playRound(stringPickTranslator(window.prompt("Enter Rock Paper Or Scissor!")), computerPlay());
-    }
+scoreLabeUpdate(){
+    
 }
+
+const pickButtons = document.querySelectorAll("#buttons button");
+pickButtons.forEach(button => {
+    button.addEventListener("click", (e) => {
+        if(player1Points == 5 || player2Points  == 5){
+            player1Points = 0;
+            player2Points = 0;
+        }
+        let textMsg = playRound(stringPickTranslator(button.getAttribute("data-pick")), computerPlay());
+        if(player1Points == 5) resText.textContent = "Congratulations, you win the 5 round Tournament!";
+        else if(player2Points == 5) resText.textContent = "Ahh, you lost the 5 round tournament, try again!";
+        else resText.textContent = textMsg;
+    })
+
+});
